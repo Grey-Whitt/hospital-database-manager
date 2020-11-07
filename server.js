26b38c95e104cbd59a0721bbd3135e88e3cc4632
Seymore Biggins <zachharris024@gmail.com>
2020-11-07 12:38:41 -0600

    connected pug engine

--- server.js
+++ server.js
@@ -3,6 +3,9 @@ const express = require('express');
 const sequelize = require('./config/connection')
 const app = express();
 const PORT = process.env.PORT || 3001;
+
+// test connection to dotenv
+console.log(process.env['USER'])
 // =========
 
 // middleware 
@@ -10,8 +13,15 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 // =========
 
+// Pug stuff
+app.set('view engine', 'pug');
+app.get('/', function (req,res) {  // this is a placeholder test to make sure pug is connecting
+    res.render('index', { title: "clinic database landing", message: "example message"});
+})
+// =========
+
 // connect to database and server
 sequelize.sync({ force: false }).then(() => {
-    app.listen(PORT, () => console.log('Now listening'));
+    app.listen(PORT, () => console.log('Now listening on port ', PORT ));
 });
 // =========
