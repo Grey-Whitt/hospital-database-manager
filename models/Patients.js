a56e74a2128e26b8f31fdf140eb11c60f6c3a5b6
greyw <greywhitt@gmail.com>
2020-11-08 16:39:51 -0600

    added models and comments

---
+++ models/Patients.js
@@ -0,0 +1,58 @@
+const { Model, DataTypes } = require('sequelize');
+const sequelize = require('../config/connection');
+const bcrypt = require('bcrypt')
+
+// create model
+class Patients extends Model {
+    checkPassword(pass) {
+        return bcrypt.compareSync(pass, this.password);
+    }
+};
+
+
+Patients.init(
+    {
+        patient_id: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            primaryKey: true,
+            autoIncrement: true
+        },
+        patient_name: {
+            type: DataTypes.STRING(40),
+            allowNull: false
+        },
+        patient_phone: {
+            type: DataTypes.INTEGER(11),
+            validate: {
+                isNumeric: true
+            }
+        },
+        patient_password: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            validate: {
+                len: [4]
+            }
+        }
+    },
+    {
+        hooks: {
+            // before a user is created, hash and salt the password password 10 times
+            async beforeCreate(newPatient) {
+                newPatient.password = await bcrypt.hash(newPatient.password, 10);
+                return newPatient;
+            },
+            // before a user is updated, hash and salt the password 10 times
+            async beforeUpdate(updtPatient) {
+                updtPatient.password = await bcrypt.hash(updtDoctor.password, 10);
+                return updtDoctor;
+            }
+        },
+
+        sequelize,
+        freezeTableName: true, //Makes model tableName and model have the same name
+        underscored: true, //auto changes fields to snake_case
+        modelName: 'patients'
+    }
+)
---
+++ models/Treatments.js
@@ -0,0 +1,18 @@
+const { Model, DataTypes } = require('sequelize');
+const sequelize = require('../config/connection');
+
+// create model
+class Treatments extends Model {};
+
+Treatments.init(
+    {
+        // define columns here
+
+    },
+    {
+        sequelize,
+        freezeTableName: true, //Makes model tableName and model have the same name
+        underscored: true, //auto changes fields to snake_case
+        modelName: 'treatments'
+    }
+)
