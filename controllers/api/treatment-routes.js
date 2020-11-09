c667f4ab14a7f6aed98c79b1d577a54bdd8357e2
tarrantrl <tarrantrl@utexas.edu>
2020-11-08 21:42:10 -0600

    add treatment api routes

--- controllers/api/treatment-routes.js
+++ controllers/api/treatment-routes.js
@@ -1,5 +1,90 @@
 const router = require('express').Router();
-const Visits = require('../../models/Visits');
+const Treatments = require('../../models/Treatments');
 const sequelize = require('../../config/connection');
+
+// GET /api/treatments
+router.get('/', (req, res) => {
+    // access model and run findall
+    Treatments.findAll()
+        .then(dbTreatmentData => res.json(dbTreatmentData))
+        .catch(err => {
+            console.log(err);
+            res.status(500).json(err);
+        });
+});
+
+// GET /api/treatment/1
+router.get('/:id', (req, res) => {
+    Treatments.findOne({
+        where: {
+            id: req.params.id
+        }
+    })
+        .then(dbTreatmentData => {
+            if (!dbTreatmentData){
+                res.status(404).json({message: 'No treatment found with this id'});
+                return;
+            }
+            res.json(dbTreatmentData);
+        })
+        .catch(err => {
+            console.log(err);
+            res.status(500).json(err);
+        });
+});
+
+// POST /api/treatments
+router.post('/', (req, res) => {
+    Treatments.create({
+        treatment_name: req.body.treatment_name,
+        treatment_cost: req.body.treatment_cost,
+        treatment_description: req.body.treatment_description
+    })
+        .then(dbTreatmentData => res.json(dbTreatmentData))
+        .catch(err => {
+            console.log(err);
+            res.status(500).json(err);
+        })
+});
+
+// PUT /api/treatments/1
+router.put('/:id', (req, res) => {
+    Treatments.update(req.body, {
+        where: {
+            id: req.params.id
+        }
+    })
+        .then(dbTreatmentData => {
+            if (!dbTreatmentData[0]){
+                res.status(404).json({message: 'No treatment found with this id'});
+                return;
+            }
+            res.json(dbTreatmentData);
+        })
+        .catch(err => {
+            console.log(err);
+            res.status(500).json(err);
+        })
+});
+
+// DELETE /api/treatments/1
+router.delete('/:id', (req, res) => {
+    Treatments.destroy({
+        where: {
+            id: req.params.id
+        }
+    })
+        .then(dbTreatmentData => {
+            if(!dbTreatmentData) {
+                res.status(404).json({message: 'No treatment found with this id'});
+                return;
+            }
+            res.json(dbTreatmentData);
+        })
+        .catch(err => {
+            console.log(err);
+            res.status(500).json(err);
+        })
+});
 
 module.exports = router;
