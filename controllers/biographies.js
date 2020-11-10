75dc9dcc20681b2e208716352c50feb78ee7daaa
spatial theory <68703408+Sarah-Safarzadeh@users.noreply.github.com>
2020-11-10 14:31:50 -0600

    header footer

--- controllers/biographies.js
+++ controllers/biographies.js
@@ -6,7 +6,7 @@ var doctors = ['Doctor1', 'Doctor2', 'Doctor3, Doctor4, Doctor5'];
 // api route needs to be created
 app.get('/doctors', function (req, res) {  
   res.render(
-    'template',
+    'biographies',
     { title: 'Meet the Doctors', header: 'Meet Your Doctors!', doctors})
 })
  
