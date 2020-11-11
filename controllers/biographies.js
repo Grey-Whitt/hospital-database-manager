35751458c7dfe75229e8fbebc2e02c8c4d19da83
spatial theory <68703408+Sarah-Safarzadeh@users.noreply.github.com>
2020-11-11 15:25:58 -0600

    html bios hdb

--- controllers/biographies.js
+++ controllers/biographies.js
@@ -1,6 +1,6 @@
 var express = require('express')  
 var app = express()  
-app.set('view engine', 'pug')
+app.set('view engine', 'handlebars')
 var doctors = ['Doctor1', 'Doctor2', 'Doctor3, Doctor4, Doctor5'];
 
 // api route needs to be created
