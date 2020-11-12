ae961a14a30da6cebd3d33d8cdac5d9451070279
Seymore Biggins <zachharris024@gmail.com>
2020-11-12 12:51:59 -0600

    fixed login styling

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ const routes = require('./controllers')
 // middleware 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(express.static(path.join(__dirname, '/public')));
+app.use(express.static(path.join(__dirname, 'public')));
 // =========
 
 //view engine
