c2225c7b8fa803953775b03627b31a82d968b326
tarrantrl <tarrantrl@utexas.edu>
2020-11-11 22:29:11 -0600

    resolve server.js merge conflict

--- server.js
+++ server.js
@@ -11,11 +11,6 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 // =========
 
-<<<<<<< HEAD
-// connect to database and server
-sequelize.sync({ force: false }).then(() => {
-    app.listen(PORT, () => console.log('Now listening on port ', PORT ));
-=======
 //view engine
 const exphbs = require('express-handlebars');
 const hbs = exphbs.create();
@@ -29,6 +24,5 @@ app.use(routes)
 // connect to database and server
 sequelize.sync({ force: false }).then(() => {
     app.listen(PORT, () => console.log('Now listening'));
->>>>>>> models
 });
 // =========
