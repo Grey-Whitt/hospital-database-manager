7f28ef716b6736ffa6269a9d796ea2cbca79906b
tarrantrl <tarrantrl@utexas.edu>
2020-11-12 17:26:16 -0600

    add doctor biographies handlebars and route

--- controllers/index.js
+++ controllers/index.js
@@ -1,11 +1,13 @@
 const router = require('express').Router();
 const apiRoutes = require('./api');
-const homeRoutes = require('./home-routes')
-const visitPageRoutes = require('./visitPage-routes')
+const homeRoutes = require('./home-routes');
+const visitPageRoutes = require('./visitPage-routes');
+const bioRoutes = require('./biographies');
 
 router.use('/api', apiRoutes);
 router.use('/', homeRoutes);
 router.use('/visits', visitPageRoutes);
+router.use('/doctors', bioRoutes);
 
 //send 404 if user goes to undefined route
 router.use((req, res) => {
