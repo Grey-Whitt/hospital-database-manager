122760151743bb7767e1d9010fe83367a250ee5d
greyw <greywhitt@gmail.com>
2020-11-12 14:18:35 -0600

    added loggin in

---
+++ public/javascript/login.js
@@ -0,0 +1,28 @@
+//logs the user in if the email and password are correct
+async function loginFormHandler(event) {
+    event.preventDefault();
+
+    const email = document.querySelector('#email').value.trim();
+    const password = document.querySelector('#password').value.trim();
+
+    if (email && password) {
+        const response = await fetch('/api/users/login', {
+            method: 'post',
+            body: JSON.stringify({
+                email,
+                password
+            }),
+            headers: { 'Content-Type': 'application/json' }
+        });
+
+        if (response.ok) {
+            
+            document.location.replace('/');
+        } else {
+            //get rid of this alert
+            alert(response.statusText);
+        }
+    }
+}
+
+document.querySelector('#loginForm').addEventListener('submit', loginFormHandler);
