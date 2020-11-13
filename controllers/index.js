0004a8c1d4f063085a4ba6429e3b46391761e13d
Seymore Biggins <zachharris024@gmail.com>
2020-11-13 13:35:49 -0600

    doctor landing framework

--- controllers/index.js
+++ controllers/index.js
@@ -3,11 +3,13 @@ const apiRoutes = require('./api');
 const homeRoutes = require('./home-routes');
 const visitPageRoutes = require('./visitPage-routes');
 const bioRoutes = require('./biographies');
+const drlandingRoutes = require('./drlanding-routes');
 
 router.use('/api', apiRoutes);
 router.use('/', homeRoutes);
 router.use('/visits', visitPageRoutes);
 router.use('/doctors', bioRoutes);
+router.use('/drlanding', drlandingRoutes);
 
 //send 404 if user goes to undefined route
 router.use((req, res) => {
