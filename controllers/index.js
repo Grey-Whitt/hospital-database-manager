fb4afcdbefc7d390c2794d4d0ecf8bd70d228980
tarrantrl <tarrantrl@utexas.edu>
2020-11-14 00:29:34 -0600

    add user add, update, and delete forms

--- controllers/index.js
+++ controllers/index.js
@@ -5,6 +5,7 @@ const visitPageRoutes = require('./visitPage-routes');
 const bioRoutes = require('./biographies');
 const drlandingRoutes = require('./drlanding-routes');
 const visitsForm = require('./visits-form');
+const userForms = require('./user-forms');
 
 router.use('/api', apiRoutes);
 router.use('/', homeRoutes);
@@ -12,6 +13,7 @@ router.use('/visits', visitPageRoutes);
 router.use('/doctors', bioRoutes);
 router.use('/drlanding', drlandingRoutes);
 router.use('/visitsform', visitsForm);
+router.use('/user-forms', userForms)
 
 //send 404 if user goes to undefined route
 router.use((req, res) => {
