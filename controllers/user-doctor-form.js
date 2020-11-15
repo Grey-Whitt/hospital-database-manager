4a46a7675de3e074abfa65e2560e7b8422798cc3
greyw <greywhitt@gmail.com>
2020-11-14 19:12:09 -0600

    adding edit visits page

--- controllers/user-doctor-form.js
+++ controllers/user-doctor-form.js
@@ -1,8 +1,17 @@
 const router = require('express').Router();
 const sequelize = require('../config/connection');
+const auth = require('../utils/auth')
+const checkRole = require('../utils/check-role')
 
-router.get('/', (req, res) => {
-    res.render('user-doctor-form');
+router.get('/', auth, checkRole, (req, res) => {
+    let doctor = false
+    if (req.session.role === 'doctor') {
+        doctor = true
+    }
+    res.render('user-doctor-form', {
+        loggedIn: req.session.loggedIn,
+        role: doctor
+    });
 });
 
 module.exports = router;
