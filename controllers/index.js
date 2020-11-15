9d80c01ff27cbf66a08beff2aa5902a2659d08e2
tarrantrl <tarrantrl@utexas.edu>
2020-11-14 23:00:37 -0600

    add edit doctor form and routes

--- controllers/index.js
+++ controllers/index.js
@@ -12,6 +12,8 @@ const docPanel = require('./doctor-panel')
 const editVisit = require('./edit-visits')
 const patientList = require('./patient-list');
 const editPatient = require('./edit-patient');
+const doctorList = require('./doctor-list');
+const editDoctor = require('./edit-doctor');
 
 router.use('/api', apiRoutes);
 router.use('/', homeRoutes);
@@ -26,6 +28,9 @@ router.use('/doctor-panel', docPanel)
 router.use('/edit-visits', editVisit)
 router.use('/patient-list', patientList);
 router.use('/edit-patient', editPatient);
+router.use('/doctor-list', doctorList);
+router.use('/edit-doctor', editDoctor)
+
 
 //send 404 if user goes to undefined route
 router.use((req, res) => {
--- public/javascript/edit-doctor.js
+++ public/javascript/edit-doctor.js
@@ -1,7 +1,7 @@
 // updates user if all fields are filled
 async function updateUserFormHandler(event) {
     event.preventDefault();
-
+    
     const user_id = window.location.toString().split('/')[
         window.location.toString().split('/').length - 1
     ];
@@ -11,6 +11,10 @@ async function updateUserFormHandler(event) {
     const email = document.querySelector('#upEmail').value.trim();
     const password = document.querySelector('#upPassword').value.trim();
 
+    const doctor_id = document.querySelector('#doctor_id').getAttribute('data-doctor-id');
+    const doctor_specialty = document.querySelector('#upSpecialty').value.trim();
+    const doctor_education = document.querySelector('#upEducation').value.trim();
+    const doctor_bio = document.querySelector('#upBio').value.trim();
 
     if (first_name && last_name && phone && email) {
         let response = '';
@@ -40,11 +44,29 @@ async function updateUserFormHandler(event) {
         }  
 
         if (response.ok) {   
-            document.location.replace('/patient-list');
+            //document.location.replace('/visits');
         } else {
             $("#errorModal").modal()
         }
     }
+
+    if (doctor_specialty && doctor_education && doctor_bio){
+        const response2 = await fetch(`/api/doctors/${doctor_id}`, {
+            method: 'put',
+            body: JSON.stringify({
+                doctor_specialty,
+                doctor_education,
+                doctor_bio
+            }),
+            headers: { 'Content-Type': 'application/json' }
+        });
+        if (response2.ok) {   
+            document.location.replace('/doctor-list');
+        } else {
+            $("#errorModal").modal()
+        }
+    }
+
 }
 
 
