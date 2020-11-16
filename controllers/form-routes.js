ce6e8c2def5d671bfc403950b1464132c89ab7ab
tarrantrl <tarrantrl@utexas.edu>
2020-11-15 20:51:19 -0600

    add visit form

--- controllers/form-routes.js
+++ controllers/form-routes.js
@@ -1,19 +1,64 @@
 const router = require('express').Router();
 const auth = require('../utils/auth')
-const checkRole = require('../utils/check-role')
+const checkRole = require('../utils/check-role');
+const { Users, Doctors, Ailments } = require('../models')
 
 router.get('/visits', auth, checkRole, (req, res) => {
-    let doctor = false
+    let doctor_role = false
     if (req.session.role === 'doctor') {
-        doctor = true
+        doctor_role = true
     }
-    res.render('visits-form', {
-        loggedIn: req.session.loggedIn,
-        doctor
-    });
+    let dataObj = {};
+    
+    Users.findAll({
+        attributes: { exclude: ['password'] },
+        where: {
+            role: "doctor"   
+        }
+    })
+    .then((doctorData) => {
+        const doctors = doctorData.map((doctor) => doctor.get({ plain: true }));
+        dataObj.doctors = doctors;
+    })
+    .catch((err) => {
+        console.log(err);
+        res.status(500).json(err);
+    })
+    .then(() => {
+        Users.findAll({
+            attributes: { exclude: ['password'] },
+            where: {
+                role: "patient"   
+            }
+        })
+        .then((patientData) => {
+            const patients = patientData.map((patient) => patient.get({ plain: true }));
+            dataObj.patients = patients;
+        })
+    })
+    .then(() => {
+        Ailments.findAll()
+        .then((ailmentData) => {
+            const ailments = ailmentData.map((ailment) => ailment.get({ plain: true }));
+            dataObj.ailments = ailments;
+        })
+    
+        .catch((err) => {
+            console.log(err);
+            res.status(500).json(err);
+        })
+    })
+    .then(() => {
+        res.render('visits-form', {
+            dataObj,
+            loggedIn: req.session.loggedIn,
+            doctor_role
+        });  
+    })
+
 });
 
-router.get('/user', auth, checkRole, (req, res) => {
+router.get('/user', (req, res) => {
     let doctor = false
     if (req.session.role === 'doctor') {
         doctor = true
