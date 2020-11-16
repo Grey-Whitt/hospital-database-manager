80cc659bfabcca060524d3b37d17531c07f6684b
tarrantrl <tarrantrl@utexas.edu>
2020-11-15 18:42:01 -0600

    fix visits and ailments association

--- models/index.js
+++ models/index.js
@@ -7,10 +7,10 @@ Users.hasMany(Visits, {foreignKey : 'visit_id'})
 Visits.belongsTo(Users, {as: 'doctor', foreignKey: 'doctor_id'})
 Visits.belongsTo(Users, {as: 'patient', foreignKey: 'patient_id'})
 
-Visits.hasOne(Ailments, {
+Visits.belongsTo(Ailments, {
     foreignKey: 'ailment_id',
     constraints: false
-})
+});
 
 Users.hasOne(Doctors, {foreignKey: 'user_id'});
 Doctors.belongsTo(Users, {foreignKey: 'user_id'});
